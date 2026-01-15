ac16940a945058492bccbd0906a2a55a37e92785
alokg-812 <alokji812@gmail.com>
2026-01-15 23:29:53 +0530

    slots geeration through python list

--- app.py
+++ app.py
@@ -312,6 +312,9 @@ def cancel_appointment(appointment_id):
     db.session.commit()
     return redirect(url_for('doctor_dashboard'))
 
+def generate_slots():
+    slots = ["08:00-08:50","09:00-09:50","10:00-10:50","11:00-11:50","12:00-12:50","13:00-13:50","14:00-14:50","15:00-15:50","16:00-16:50"]
+    return slots
 
 if __name__ == '__main__':
     with app.app_context():
