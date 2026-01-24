19c641f70a269428ce1510092ce0423fd50d6f9d
alokg-812 <alokji812@gmail.com>
2026-01-25 00:14:36 +0530

    patient dashboard code written

--- app.py
+++ app.py
@@ -316,6 +316,17 @@ def generate_slots():
     slots = ["08:00-08:50","09:00-09:50","10:00-10:50","11:00-11:50","12:00-12:50","13:00-13:50","14:00-14:50","15:00-15:50","16:00-16:50"]
     return slots
 
+@app.route('/patient_dashboard')
+def patient_dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    user = User.query.get(session['user_id'])
+    departments = Department.query.all()
+    appointments = Appointment.query.filter_by(patient_id=user.id, status="Booked").order_by(Appointment.date).all()
+    print(departments)
+    print(appointments)
+    return render_template('patient/patient_dashboard.html', user=user, departments=departments, appointments=appointments)
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
